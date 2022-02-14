83eb8cf13b9d9e1dfa41d4c2ed5a8e199ac669f6
Gabriel Olivério <gabrieloliverio.py@gmail.com>
2022-02-14 17:06:14 -0300

    add lunarvim config file

---
+++ config.lua
@@ -0,0 +1,38 @@
+vim.opt.relativenumber = true
+vim.g.bufferline = {
+  icons = "both"
+}
+
+lvim.log.level = "warn"
+lvim.format_on_save = true
+lvim.colorscheme = "onedarker"
+lvim.leader = "space"
+
+lvim.keys.normal_mode = {
+  ["<S-L>"] = false,
+  ["<S-H>"] = false,
+  ["<S-J>"] = ":BufferNext<CR>",
+  ["<S-K>"] = ":BufferPrevious<CR>",
+  ["<C-S>"] = ":w<CR>",
+}
+
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "php",
+  "typescript",
+  "css",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
+
+lvim.autocommands.custom_groups = {
+  { "BufWinEnter", "*.php", "setlocal ts=4 sw=4" },
+  { "BufWinEnter", "*.js", "setlocal ts=4 sw=4" },
+}
